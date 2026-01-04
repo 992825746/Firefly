f069ebef4bbaf0d1eeefa378e42edc49fa98c425
Europa <qq992825746@gmail.com>
2026-01-05 00:04:53 +0800

    Update siteConfig.ts

--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -67,7 +67,7 @@ export const siteConfig: SiteConfig = {
 	},
 
 	// 站点开始日期，用于统计运行天数
-	siteStartDate: "2026-01-03",
+	siteStartDate: "2025-05-01",
 
 	// 文章页底部的"上次编辑时间"卡片开关
 	showLastModified: true,
